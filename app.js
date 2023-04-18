caa3105fccca671a12d20ffd6b83484779de783d
Ian Mason <ianmason5@googlemail.com>
2023-04-18 12:02:18 +0100

    Done

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 
+// Serve static files (for CSS, Images etc.)
+app.use(express.static("public"));
+
 // Route to the home page
 app.get("/", (req, res) => {
-  console.log("Request to the home page");
-  res.send("Welcome to the home page");
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+// About page
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+// Works page
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+// Gallery page
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
 });
 
 // Start the server
